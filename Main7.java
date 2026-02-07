4e94b0bed857b0af55746aba97897fdd3eda3a11
kumi-ikematsu <51ki9m@gmail.com>
2026-02-07 12:26:22 +0900

    Add files via upload

---
+++ Main7.java
@@ -0,0 +1,12 @@
+public class Main7 {
+    public static void main(String[] args) {
+        String lastname = "山田";
+        String firstname = "太郎";
+        int age = 16;
+        String email = "yamada@example.com";
+        
+        System.out.println(" 名前:" + lastname + firstname);
+        System.out.println("年齢" + age + "歳");
+        System.out.println("メールアドレス:" + email);
+    }
+}
---
+++ Main8.java
@@ -0,0 +1,12 @@
+public class Main8 {
+    public static void main(String[] args) {
+        //見出しを表示
+        System.out.println("===== 自己紹介 =====");
+        //名前を表示
+        System.out.println("名前:池松玖美");
+        //出身地を表示
+        System.out.println("出身地:熊本県");
+        //好きな食べ物を表示
+        System.out.println("好きな食べ物:寿司");
+    }
+}
