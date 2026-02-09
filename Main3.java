a8c7a4edc8abca3283e524422daace84b8de3b83
kumi-ikematsu <51ki9m@gmail.com>
2026-02-09 11:38:41 +0900

    Add files via upload
    訂正版のファイルです
    宜しくお願い致します

--- Main3.java
+++ Main3.java
@@ -1,11 +1,12 @@
 /**　このクラスは、Hello World　を表示するサンプルプログラム **/
 public class Main3 {
+    //コンソールにメッセージを表示
     public static void main(String[] args) {
-        //コンソールにメッセージを表示
-        System.out.println("Hello, World!");
         /*
         * プログラム終了
         * mainメソッドに書いた処理は、上から順に実行される
         */
+        System.out.println("Hello, World!");
+        
     }
 }
--- Main7.java
+++ Main7.java
@@ -1,12 +1,12 @@
 public class Main7 {
     public static void main(String[] args) {
-        String lastname = "山田";
-        String firstname = "太郎";
+        String lastName = "山田";
+        String firstName = "太郎";
         int age = 16;
         String email = "yamada@example.com";
         
-        System.out.println(" 名前:" + lastname + firstname);
-        System.out.println("年齢" + age + "歳");
-        System.out.println("メールアドレス:" + email);
+        System.out.println(" 名前: " + lastName + " " + firstName);
+        System.out.println("年齢: " +  age );
+        System.out.println("メールアドレス: " + email);
     }
 }
