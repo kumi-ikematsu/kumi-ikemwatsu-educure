e66c7a008b56b7f59aa3b27b91cb7d52ab7a85d6
kumi-ikematsu <51ki9m@gmail.com>
2026-02-10 01:50:57 +0900

    Add files via upload

---
+++ 2-3/Main5.java
@@ -0,0 +1,10 @@
+/**Javadocの位置（コードの前）を確認するためのプログラム
+ */
+public class Main5 {
+    public static void main(String[] args) {
+        // 数学の点数を変数に代入する
+        int mathScore = 85;
+        // 点数をコンソールに表示する
+        System.out.println("数学の点数: " + mathScore);
+    }
+}
---
+++ 2-3/Main8.java
@@ -0,0 +1,20 @@
+/**
+ * 名前・出身地・好きな食べ物を変数に格納し、
+ * その内容を表示するプログラム。
+ */
+public class Main8 {
+    public static void main(String[] args) {
+        //見出しを表示
+        System.out.println("===== 自己紹介 =====");
+        //名前を表示
+        String name = "池松 玖美";
+        //出身地を表示
+        String hometown = "福岡";
+        //好きな食べ物を表示
+        String favoriteFood = "ラーメン";
+        
+        System.out.println("名前: " + name);
+        System.out.println("出身地: " + hometown);
+        System.out.println("好きな食べ物: " + favoriteFood);
+    }
+}
