140e99b208d4ef6456f1d8bdf33b567e7264018c
kumi-ikematsu <51ki9m@gmail.com>
2026-02-15 10:18:38 +0900

    Add files via upload
    2-12追加フォルダ

--- Main3.java
+++ Main3.java
@@ -1,12 +1,22 @@
-/**　このクラスは、Hello World　を表示するサンプルプログラム **/
 public class Main3 {
-    //コンソールにメッセージを表示
     public static void main(String[] args) {
-        /*
-        * プログラム終了
-        * mainメソッドに書いた処理は、上から順に実行される
-        */
-        System.out.println("Hello, World!");
-        
+
+        int height = 5; // ピラミッドの高さ
+
+        for (int row = 1; row <= height; row++) {
+
+            // 空白
+            for (int s = 0; s < height - row; s++) {
+                System.out.print(" ");
+            }
+
+            // ★（奇数個）
+            for (int star = 0; star < 2 * row - 1; star++) {
+                System.out.print("*");
+            }
+
+            // 改行
+            System.out.println();
+        }
     }
 }
