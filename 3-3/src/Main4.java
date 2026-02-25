470a9fb74decf59b1678987c425e91c9452b0e59
kumi-ikematsu <51ki9m@gmail.com>
2026-02-25 21:10:21 +0900

    Main4.java を更新

--- 3-3/src/Main4.java
+++ 3-3/src/Main4.java
@@ -1,7 +1,8 @@
 public class Main4 {
     public static void main(String[] args) {
 
-        Weapon weapon = new Weapon("伝説の剣", 100);
+        Weapon weapon = new Weapon();
+        weapon.setWeapon("伝説の剣", 100);
 
         for (int i = 0; i < 10; i++) {
             weapon.use();
