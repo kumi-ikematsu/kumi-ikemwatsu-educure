a71a2d108caf22e593718473cba11315b1bef778
kumi-ikematsu <51ki9m@gmail.com>
2026-03-13 07:23:49 +0900

    Delete 4-18/src directory

--- 4-18/src/Divider.java
+++
@@ -1,9 +0,0 @@
-public class Divider {
-
-    public double divide(double a, double b) {
-        if (b == 0.0) {
-            throw new IllegalArgumentException("ゼロでは除算できません");
-        }
-        return a / b;
-    }
-}
