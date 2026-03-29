d209dbd17669a078c8a1fceaea72fc48eeefec73
kumi-ikematsu <51ki9m@gmail.com>
2026-03-29 15:30:44 +0900

    Update UpdateProduct.java

--- 5-14/src/UpdateProduct.java
+++ 5-14/src/UpdateProduct.java
@@ -3,21 +3,21 @@ import java.sql.PreparedStatement;
 
 public class UpdateProduct {
 
-    public static void updateStock(Connection conn, int addStock, int productId) {
+    public static void update(Connection conn, int newPrice, int productId) {
 
         try {
-            String sql = "UPDATE products SET stock = stock + ? WHERE product_id = ?";
+            String sql = "UPDATE products SET price = ? WHERE product_id = ?";
 
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, addStock);
+            pstmt.setInt(1, newPrice);
             pstmt.setInt(2, productId);
 
             int result = pstmt.executeUpdate();
 
             if (result > 0) {
-                System.out.println("在庫更新が完了しました。");
+                System.out.println("更新成功");
             } else {
-                System.out.println("該当する商品が見つかりませんでした。");
+                System.out.println("対象データなし");
             }
 
         } catch (Exception e) {
