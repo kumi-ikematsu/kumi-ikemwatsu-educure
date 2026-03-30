f6ee193df38fa0d79077b7d256941413ef0e4804
kumi-ikematsu <51ki9m@gmail.com>
2026-03-30 16:48:12 +0900

    Update SearchProduct.java

--- 5-14/src/SearchProduct.java
+++ 5-14/src/SearchProduct.java
@@ -4,48 +4,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class SearchProduct {
-
     public static void main(String[] args) {
 
-        Connection conn = null;
+        String url = "jdbc:postgresql://localhost:5432/educure_db";
+        String user = "postgres";
+        String password = "password";
 
-        try {
-            String url = "jdbc:postgresql://localhost:5432/educure_db";
-            String user = "postgres";
-            String password = "password";
+        String sql = "SELECT product_name, price FROM products WHERE price >= ?";
 
-            conn = DriverManager.getConnection(url, user, password);
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            int productId = 1;
+            pstmt.setInt(1, 100000);
 
-            String sql = "SELECT * FROM products WHERE product_id = ?";
+            try (ResultSet rs = pstmt.executeQuery()) {
 
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, productId);
-
-            ResultSet rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                System.out.println("商品ID: " + rs.getInt("product_id"));
-                System.out.println("商品名: " + rs.getString("name"));
-                System.out.println("価格: " + rs.getInt("price"));
-                System.out.println("在庫: " + rs.getInt("stock"));
-            } else {
-                System.out.println("商品が見つかりませんでした。");
+                while (rs.next()) {
+                    System.out.println(
+                            "商品名：" + rs.getString("product_name") +
+                            "、価格：" + rs.getInt("price")
+                    );
+                }
             }
 
         } catch (Exception e) {
             e.printStackTrace();
-
-        } finally {
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 }
