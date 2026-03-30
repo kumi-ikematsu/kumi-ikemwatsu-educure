3677d52dfede8a4be3c90c99524660ce284e75a9
kumi-ikematsu <51ki9m@gmail.com>
2026-03-30 12:44:58 +0900

    Update UpdateProduct.java

--- 5-14/src/UpdateProduct.java
+++ 5-14/src/UpdateProduct.java
@@ -1,11 +1,24 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class UpdateProduct {
 
-    public static void update(Connection conn, int newPrice, int productId) {
+    public static void main(String[] args) {
+
+        Connection conn = null;
 
         try {
+            String url = "jdbc:postgresql://localhost:5432/educure_db";
+            String user = "postgres";
+            String password = "password";
+
+            conn = DriverManager.getConnection(url, user, password);
+            conn.setAutoCommit(false);
+
+            int newPrice = 2000;
+            int productId = 1;
+
             String sql = "UPDATE products SET price = ? WHERE product_id = ?";
 
             PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -20,8 +33,29 @@ public class UpdateProduct {
                 System.out.println("対象データなし");
             }
 
+            conn.commit();
+
         } catch (Exception e) {
+
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
             e.printStackTrace();
+
+        } finally {
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
