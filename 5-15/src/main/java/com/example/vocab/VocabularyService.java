0859ba5f6152cdc44da3ba4d4ee68d1b6256de32
kumi-ikematsu <51ki9m@gmail.com>
2026-03-31 17:25:04 +0900

    Update VocabularyService.java

--- 5-15/src/main/java/com/example/vocab/VocabularyService.java
+++ 5-15/src/main/java/com/example/vocab/VocabularyService.java
@@ -1,30 +1,3 @@
-package com.example.vocab;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
-public class VocabularyService {
-
-    private VocabularyDAO dao;
-
-    public VocabularyService(Connection con) {
-        this.dao = new VocabularyDAO(con);
-    }
-
-    public boolean add(String word, String meaning) throws SQLException {
-        return dao.insert(word, meaning) > 0;
-    }
-
-    public List<Vocabulary> getAll() throws SQLException {
-        return dao.findAll();
-    }
-
-    public boolean update(String word, String meaning) throws SQLException {
-        return dao.update(word, meaning) > 0;
-    }
-
-    public boolean delete(String word) throws SQLException {
-        return dao.delete(word) > 0;
-    }
+public boolean add(String word, String meaning) throws Exception {
+    return dao.insert(word, meaning) > 0;
 }
