b8b20f73eb577a10178bec7c0dd6990ea33bfc34
kumi-ikematsu <51ki9m@gmail.com>
2026-03-31 15:50:59 +0900

    Create Vocabulary.java

---
+++ 5-15/src/main/java/com/example/vocab/Vocabulary.java
@@ -0,0 +1,20 @@
+package com.example.vocab;
+
+public class Vocabulary {
+
+    private String word;
+    private String meaning;
+
+    public Vocabulary(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+}
