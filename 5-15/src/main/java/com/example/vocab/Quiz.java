f24e57a076efd4b53e620c6fde50260188f88b08
kumi-ikematsu <51ki9m@gmail.com>
2026-04-02 08:45:28 +0900

    Update and rename QuizService.java to Quiz.java

---
+++ 5-15/src/main/java/com/example/vocab/Quiz.java
@@ -0,0 +1,40 @@
+package com.example.vocab;
+
+import java.util.List;
+import java.util.Random;
+
+public class Quiz {
+
+    private WordManager wordManager;
+    private int score;
+    private int totalQuestions;
+
+    public Quiz(WordManager wordManager) {
+        this.wordManager = wordManager;
+        this.score = 0;
+        this.totalQuestions = 0;
+    }
+
+    public boolean checkAnswer(Word word, String answer) {
+        totalQuestions++;
+        if (answer.equalsIgnoreCase(word.getJapanese())) {
+            score++;
+            return true;
+        }
+        return false;
+    }
+
+    public Word getRandomWord() {
+        List<Word> list = wordManager.getWords();
+        if (list.isEmpty()) return null;
+        return list.get(new Random().nextInt(list.size()));
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+}
