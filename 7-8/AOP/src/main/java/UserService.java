ac556c4e12e6ef84c3fcded261f98efba0f82154
kumi-ikematsu <51ki9m@gmail.com>
2026-04-06 10:06:43 +0900

    Create UserService.java

---
+++ 7-8/AOP/src/main/java/UserService.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    public void getUserDetails() {
+        System.out.println("User details accessed.");
+    }
+}
