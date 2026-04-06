60faf8bd457802f11295c731d011acac2cc9eba9
kumi-ikematsu <51ki9m@gmail.com>
2026-04-06 12:39:14 +0900

    Add UserController with user endpoint

---
+++ 7-11/src/main/java/UserController.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user")
+    public String user(Model model) {
+        model.addAttribute("username", "John Doe");
+        return "user";
+    }
+}
