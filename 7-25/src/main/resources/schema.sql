753848b92fe921ade222be3b6609f64cbcd29de9
kumi-ikematsu <51ki9m@gmail.com>
2026-04-07 14:08:19 +0900

    Update schema.sql

--- 7-25/src/main/resources/schema.sql
+++ 7-25/src/main/resources/schema.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS book_categories;
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
@@ -6,4 +8,18 @@ CREATE TABLE books (
     author VARCHAR(255) NOT NULL,
     price INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE book_categories (
+    book_id INT,
+    category_id INT,
+    PRIMARY KEY (book_id, category_id),
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
