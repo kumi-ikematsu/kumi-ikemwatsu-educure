8e06f986a493f5269f913a94c70bd01d82505112
kumi-ikematsu <51ki9m@gmail.com>
2026-04-07 09:02:04 +0900

    Create PhoneNumberForm.java

---
+++ 7-22/problem2/src/main/java/com/example/PhoneNumberForm.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class PhoneNumberForm {
+
+    @ValidPhoneNumber
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
