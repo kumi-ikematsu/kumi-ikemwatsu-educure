af22531f08badcd42c4b36335e4d33ac1dcd28c5
kumi-ikematsu <51ki9m@gmail.com>
2026-04-07 13:38:50 +0900

    Create Category.java

---
+++ 7-25/src/main/java/jp/educure/bookmanagement/entity/Category.java
@@ -0,0 +1,18 @@
+package jp.educure.bookmanagement.entity;
+
+import java.time.LocalDateTime;
+
+public class Category {
+    private int id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
