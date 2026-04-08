aef2cd4370250ec4c2d09db790b85387dc9b8c7e
kumi-ikematsu <51ki9m@gmail.com>
2026-04-08 09:51:50 +0900

    Update WordController.java

--- 7-34/wordapp/src/main/java/com/example/wordapp/controller/WordController.java
+++ 7-34/wordapp/src/main/java/com/example/wordapp/controller/WordController.java
@@ -1,27 +1,38 @@
-// TODO: @Valid と BindingResult を追加し、エラー時は register.html を返してください
-@PostMapping("/register")
-public String registerWord(
-        ____________ @ModelAttribute Word word,
-        ____________ bindingResult) {
+@Controller
+public class WordController {
 
-    if (____________.____________()) {
+    @Autowired
+    WordService wordService;
+
+    // TODO: 一覧画面を表示するGETメソッドを実装してください
+    // パス：/words / 処理：全単語をModelに"words"として追加し words.html を返す
+    @GetMapping("/words")
+    public String showWordList(____________ model) {
+        model.addAttribute("____________", ____________);
         return "____________";
     }
 
-    wordService.registerWord(word);
-    return "redirect:/words";
-}
-
-// TODO: 同様に @Valid と BindingResult を追加し、エラー時は edit.html を返してください
-@PostMapping("/words/update")
-public String updateWord(
-        ____________ @ModelAttribute Word word,
-        ____________ bindingResult) {
-
-    if (____________) {
+    // TODO: 編集フォームを表示するGETメソッドを実装してください
+    // パス：/words/edit/{id} / 処理：idで単語を取得しModelに"word"として追加し edit.html を返す
+    @GetMapping("/words/edit/____________")
+    public String showEditForm(@____________("id") int id, Model model) {
+        model.addAttribute("____________", ____________);
         return "____________";
     }
 
-    wordService.updateWord(word);
-    return "redirect:/words";
+    // TODO: 更新処理を行うPOSTメソッドを実装してください
+    // パス：/words/update / 処理：updateWordを呼び出し /words にリダイレクト
+    @PostMapping("/words/update")
+    public String updateWord(@ModelAttribute Word word) {
+        ____________;
+        return "____________";
+    }
+
+    // TODO: 削除処理を行うPOSTメソッドを実装してください
+    // パス：/words/delete/{id} / 処理：deleteWordを呼び出し /words にリダイレクト
+    @____________("/words/delete/{id}")
+    public String deleteWord(@____________("id") ____________ id) {
+        ____________;
+        return "redirect:/words";
+    }
 }
