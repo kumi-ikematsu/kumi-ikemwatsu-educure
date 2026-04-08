0f061237307e993d0ae2b64725f12c966d68afde
kumi-ikematsu <51ki9m@gmail.com>
2026-04-08 16:18:28 +0900

    Update ToDoForm.java

--- 7-34/src/main/java/jp/educure/webapp/form/ToDoForm.java
+++ 7-34/src/main/java/jp/educure/webapp/form/ToDoForm.java
@@ -5,25 +5,13 @@ import jakarta.validation.constraints.Size;
 
 public class ToDoForm {
 
-    private Integer id;
-
     @NotBlank(message = "ToDoは必須です。")
     private String todo;
 
     @Size(min = 1, max = 100, message = "詳細は1〜100文字以内で入力してください。")
     private String detail;
 
-    public ToDoForm() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
+    // Getter & Setter
     public String getTodo() {
         return todo;
     }
