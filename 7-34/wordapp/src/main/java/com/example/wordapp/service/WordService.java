7c67abac012c85e7a786b22a030d22829b41f5ea
kumi-ikematsu <51ki9m@gmail.com>
2026-04-08 09:37:22 +0900

    Create WordService.java

---
+++ 7-34/wordapp/src/main/java/com/example/wordapp/service/WordService.java
@@ -0,0 +1,14 @@
+@Service
+public class WordService {
+
+    // TODO: WordMapperをDIしてください
+    ____________ WordMapper wordMapper;
+
+    // TODO: 単語を登録するメソッドを実装してください
+    // メソッド名：registerWord
+    // 引数：Word オブジェクト
+    // 処理：wordMapperのinsertWordを呼び出す
+    public void registerWord(____________) {
+        ____________;
+    }
+}
