fe320ac761f6da586a39b03298e156d021847af8
kumi-ikematsu <51ki9m@gmail.com>
2026-04-09 15:29:05 +0900

    Create Word.java

---
+++ 7-35/src/main/java/jp/educure/webapp/entity/Word.java
@@ -0,0 +1,52 @@
+package jp.educure.webapp.entity;
+
+import java.time.LocalDateTime;
+
+public class Word {
+
+    private Integer id;
+    private String english;
+    private String japanese;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getJapanese() {
+        return japanese;
+    }
+
+    public void setJapanese(String japanese) {
+        this.japanese = japanese;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
