5d77d1987643b116ed0d3775790bcad7c08cc8cb
kumi-ikematsu <51ki9m@gmail.com>
2026-04-09 14:56:27 +0900

    Delete 7-35 directory

--- 7-35/wordapp/src/main/java/com/example/wordapp/mapper/WordMapper.java
+++
@@ -1,9 +0,0 @@
-@Mapper
-public interface WordMapper {
-
-    // TODO: 単語を1件登録するメソッドを定義してください
-    // メソッド名：insertWord
-    // 引数：Word オブジェクト
-    // 戻り値：なし（void）
-    ____________ insertWord(____________ word);
-}
